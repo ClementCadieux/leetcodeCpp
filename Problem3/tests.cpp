a2bb1c64fe2e75c099c28e23ae8cadcc77656c79
Clement Cadieux <clement.cadieux@fdmgroup.com>
2025-03-15 11:33:08 -0400

    manual test fix

--- Problem3/tests.cpp
+++ Problem3/tests.cpp
@@ -1,30 +1,39 @@
 #include "solution.h"
-#include <gtest/gtest.h>
+#include <iostream>
+#include <string>
 
-class SolutionTest : public ::testing::Test {
-protected:
-    Solution solution;
+class SolutionTest {
+public:
+    void runTest1() {
+        Solution solution;
+        std::string s = "abcabcbb";
+        int result = solution.lengthOfLongestSubstring(s);
+        std::cout << "TestCase1: Expected 3, Got " << result << std::endl;
+    }
+
+    void runTest2() {
+        Solution solution;
+        std::string s = "bbbbb";
+        int result = solution.lengthOfLongestSubstring(s);
+        std::cout << "TestCase2: Expected 1, Got " << result << std::endl;
+    }
+
+    void runTest3() {
+        Solution solution;
+        std::string s = "pwwkew";
+        int result = solution.lengthOfLongestSubstring(s);
+        std::cout << "TestCase3: Expected 3, Got " << result << std::endl;
+    }
+
+    void runAllTests() {
+        runTest1();
+        runTest2();
+        runTest3();
+    }
 };
 
-TEST_F(SolutionTest, TestCase1) {
-    std::string s = "abcabcbb";
-    int result = solution.lengthOfLongestSubstring(s);
-    EXPECT_EQ(result, 3);
-}
-
-TEST_F(SolutionTest, TestCase2) {
-    std::string s = "bbbbb";
-    int result = solution.lengthOfLongestSubstring(s);
-    EXPECT_EQ(result, 1);
-}
-
-TEST_F(SolutionTest, TestCase3) {
-    std::string s = "pwwkew";
-    int result = solution.lengthOfLongestSubstring(s);
-    EXPECT_EQ(result, 3);
-}
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+int main() {
+    SolutionTest testSuite;
+    testSuite.runAllTests();
+    return 0;
 }
