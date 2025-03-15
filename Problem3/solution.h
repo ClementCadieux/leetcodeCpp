594f11025affa0a252d4a7111c702b1c5630a135
Clement Cadieux <clement.cadieux@fdmgroup.com>
2025-03-15 10:51:26 -0400

    setup

---
+++ Problem3/solution.h
@@ -0,0 +1,15 @@
+#include <string>
+#ifndef SOLUTION_H
+#define SOLUTION_H
+
+using namespace std;
+
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+    }
+};
+
+#endif // SOLUTION_H
