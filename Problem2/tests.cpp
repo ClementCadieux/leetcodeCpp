7da47c78ff3c9a6889b6833a84ff486bc07ac189
Clement Cadieux <clement.cadieux@fdmgroup.com>
2025-03-15 10:06:19 -0400

    Initial setup

---
+++ Problem2/tests.cpp
@@ -0,0 +1,45 @@
+#include "Solution.h"
+#include <iostream>
+#include <vector>
+
+template <typename T>
+class tests
+{
+public:
+    void runTest1()
+    {
+        Solution sol;
+
+
+
+        std::cout << std::endl;
+    }
+
+    void runTest2()
+    {
+        Solution sol;
+
+        
+
+        std::cout << std::endl;
+    }
+
+    void runTest3()
+    {
+        Solution sol;
+
+        
+        
+        std::cout << std::endl;
+    }
+};
+
+int main()
+{
+    tests<int> testSuite;
+    testSuite.runTest1();
+    testSuite.runTest2();
+    testSuite.runTest3();
+
+    return 0;
+}
