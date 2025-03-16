1ca8b4f20a64ebd0423d15ad9e3c4433f560bd3a
Clement Cadieux <clement.cadieux@fdmgroup.com>
2025-03-15 22:10:54 -0400

    setup

---
+++ Problem6/solution.h
@@ -0,0 +1,16 @@
+#include <string>
+#ifndef SOLUTION_H
+#define SOLUTION_H
+
+using namespace std;
+
+class Solution
+{
+public:
+    string convert(string s, int numRows)
+    {
+
+    }
+};
+
+#endif // SOLUTION_H
