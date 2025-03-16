f7bf8ee93a2a3069108683223844f452647fada9
Clement Cadieux <clement.cadieux@fdmgroup.com>
2025-03-15 22:44:30 -0400

    Problem 6 done

--- Problem6/solution.h
+++ Problem6/solution.h
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 #ifndef SOLUTION_H
 #define SOLUTION_H
 
@@ -9,7 +10,41 @@ class Solution
 public:
     string convert(string s, int numRows)
     {
+        std::vector<string> rows = {};
 
+        for (int i = 0; i < numRows; i++)
+        {
+            /* code */
+            rows.push_back("");
+        }
+
+        int direction = 1;
+
+        int rowsIdx = 0;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            /* code */
+            rows[rowsIdx] += s[i];
+
+            rowsIdx += direction;
+
+            if (rowsIdx == 0 && direction == -1){
+                direction = 1;
+            } else if (rowsIdx == numRows - 1 && direction == 1){
+                direction = -1;
+            }
+        }
+
+        string res = "";
+
+        for (int i = 0; i < rows.size(); i++)
+        {
+            /* code */
+            res += rows[i];
+        }
+
+        return res;        
     }
 };
 
